1c4e67626c0273299b97ef11290cfd016b5e153e
Gabriel Rockwell <Rockwell_500@icloud.com>
2019-11-16 19:45:20 -0700

    added favicon

--- src/components/layout.js
+++ src/components/layout.js
@@ -35,10 +35,11 @@ const Layout = ({ children }) => {
         }}
       >
         <main>{children}</main>
-        <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
+        <footer style={{
+          alignSelf: "center",
+          display: "flex",
+        }}>
+          © {new Date().getFullYear()}, Rockwell Development
         </footer>
       </div>
     </>
